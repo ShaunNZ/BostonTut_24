6d2f5dca540e793195ebb98388a7a99c1365f6a4
ShaunNZ <srugh@xtra.co.nz>
2018-03-29 08:18:13 +1300

    commit

---
+++ app/src/main/java/com/example/shaun/bostontut_24/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.shaun.bostontut_24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView myView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Comment change
+
+}
